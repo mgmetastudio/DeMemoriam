abf0b4642b5078d4b3f2aff28561ad192125e5aa
onetwothreeswap <onetwothreeswap@gmail.com>
2023-02-23 17:13:27 +0200

    Added backend API with simple user registration to master branch

---
+++ backend/dememoriam_api/urls.py
@@ -0,0 +1,36 @@
+"""dememoriam_api URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+from rest_framework import routers
+from dememoriam_api import settings
+from dememoriam_api.apps.users.views import ResendEmailVerificationView, UserProfileView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('rest-auth/', include('dj_rest_auth.urls')),
+    path('rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+
+    path('rest-auth/registration/resend-email/', ResendEmailVerificationView.as_view(), name="rest_resend_email"),
+    path('password-reset/confirm/<uidb64>/<token>/', TemplateView.as_view(), name='password_reset_confirm'),
+    path('account-confirm-email/<key>/', TemplateView.as_view(), name='account_confirm_email'),
+
+    path('user/profile/', UserProfileView.as_view()),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
