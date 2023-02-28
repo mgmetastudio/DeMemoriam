19290904a88df3a203f171f7b1aad9a1e6e862c5
Jonas Levinskas <levinskas.jonas@gmail.com>
2023-02-28 16:11:56 +0200

    Changed about_me field type

--- backend/dememoriam_api/apps/users/models.py
+++ backend/dememoriam_api/apps/users/models.py
@@ -9,7 +9,7 @@ from rest_framework import serializers
 class User(AbstractUser):
     flow_address = models.CharField(max_length=64, null=True, blank=True)
     avatar = ResizedImageField(size=[500, 500], crop=['middle', 'center'], upload_to='avatars', blank=True, null=True)
-    about_me = models.CharField(max_length=164, null=True, blank=True)
+    about_me = models.TextField(null=True, blank=True)
     country = CountryField(blank=True)
     birth_date = models.DateField(null=True, blank=True)
 
